b092f9c4ecb1ba2fa1aeecab8758fdf92f7fb02c
Petr Sorad <petr_sorad@cz.ibm.com>
2020-01-14 14:23:30 +0100

    Coco - get finished, put modified to return  array resp

--- src/main.js
+++ src/main.js
@@ -12,45 +12,95 @@ app.use(express.json());
 app.put('/coco', function(req, res) {
     if (Array.isArray(req.body)) {
         logger.info('Processing array: ',JSON.stringify(req.body));
-        let response = [];
+        let promises = [];
         req.body.forEach(function(element) {
-            logger.info('  Processing element: ',JSON.stringify(element));
-            if (true) {
-                const right = new Right(element);
-                right.save().then(function() {
-                    logger.info('Element ',JSON.stringify(element),' saved.');
-                    response.push(right);
-                }).catch(function(error) {
-                    logger.error('Element save caused error: ', error);
-                    response.push('Request item: ',JSON.stringify(element), ' finished with error: ', error);
-                });
-            }
+            promises.push(put_single(element).then(function(inst) {
+                return (inst);
+            }))
+
+            // // TODO: check input attributes
+            // if (true) {
+            //     logger.info('  Processing element: ',JSON.stringify(element));
+                
+            //     Right.findOne({$and: [{ role : element.role }, { content_id: element.content_id }]}, function(err, res) {
+            //         if(err) {
+            //             logger.error('findById caused error: ',JSON.stringify(err));
+            //         } else {
+            //             logger.info('findById found doc: ',JSON.stringify(res));
+            //             if(!res) {
+            //                 res = new Right(element);
+            //             } else {
+            //                 res.read = element.read;
+            //                 res.write = element.write;
+            //             }
+            //             res.save().then(function(res2) {
+            //                 logger.info('Element ',JSON.stringify(element),' saved as doc ',JSON.stringify(res2));
+            //                 response.push(res2);
+            //             }).catch(function(error) {
+            //                 logger.error('Element save caused error: ', error);
+            //                 response.push('Request item: ',JSON.stringify(element), ' finished with error: ', error);
+            //             });
+            //         }
+            //     });
+            // }
+
+
         });
-        logger.info('sending response: ', JSON.stringify(response));
-        res.send(response);
+        Promise.all(promises).then(function(resp){
+            logger.info('sending response: ', JSON.stringify(resp));
+            res.send(resp);
+        });
     } else {
         logger.error('Input: ', JSON.stringify(req.body), ' is NOT array!');
         res.send('Array input expected!');
     }
-    // TODO: implement upsert of new/existing right - examine existing content_id X role
-    // decompose array: forEach:
-    // chk input prms
-    // compose model record
-    // save; TBD: versioning
 });
+
+put_single = function (element) {
+    return new Promise(function(resolve, reject) {
+        // TODO: check input attributes
+        if (true) {
+            logger.info('  Processing element: ',JSON.stringify(element));
+            
+            Right.findOne({$and: [{ role : element.role }, { content_id: element.content_id }]}, function(err, res) {
+                if(err) {
+                    logger.error('findById caused error: ',JSON.stringify(err));
+                } else {
+                    logger.info('findById found doc: ',JSON.stringify(res));
+                    if(!res) {
+                        res = new Right(element);
+                    } else {
+                        res.read = element.read;
+                        res.write = element.write;
+                    }
+                    res.save().then(function(res2) {
+                        logger.info('Element ',JSON.stringify(element),' saved as doc ',JSON.stringify(res2));
+                        resolve(res2);
+                    }).catch(function(error) {
+                        logger.error('Element save caused error: ', error);
+                        reject('Request item: ',JSON.stringify(element), ' finished with error: ', error);
+                    });
+                }
+            });
+        }
+    });
+}
 
 // req: { 'content_id': '<value>' } or { 'roles': ['<role1>', '<role2>',..]]}
 // TODO: return last version of doc
 // response:
-// {
-// 	'roles': [ 'admin', 'provisioner', .. ],       // zopakovany list z req
-// 	'allowed_content': [
-// 		'id1': { 'read': true, 'write': false },
-// 		'id2': { 'read': true, 'write': false },
-// 		'id3': { 'read': true, 'write': true },
-// 		..
-// 	]
-// }
+// [
+// 	  {
+//      "content_id": "123456",
+//      "read": true,
+//      "write": false
+//    },
+//    {
+//      "content_id": "123457",
+//      "read": true,
+//      "write": false
+//    }, ..
+//   ]
 app.get('/coco', function (req, res) {
     logger.log('req.body: ',JSON.stringify(req.body));
     let filter = {};
@@ -72,11 +122,19 @@ app.get('/coco', function (req, res) {
 
     Right.find(filter).then(function (result) {
         logger.info('result: ',JSON.stringify(result));
+        let resp = [];
+        result.forEach(function(rec){
+            let resp_element = {};
+            
+            resp_element.content_id = rec.content_id;
+            resp_element.read = rec.read;
+            resp_element.write = rec.write;
+            resp.push(resp_element);
+        });
         // TODO: process result => resp
         // fill roles
         // iterate returned documents and U'em into final structure
-        res.send(JSON.stringify(result));
-        // TODO: replace above by res.send(resp);
+        res.send(resp);
     }).catch(function (error) {
         res.status(500).send(error);
     });
